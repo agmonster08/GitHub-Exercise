b05bc3e609dfdd865c0dccee9b62017c4dc0e8ab
agmonster08 <26757790+agmonster08@users.noreply.github.com>
2024-10-23 17:07:04 -0400

    Version 1.0.0 - Red

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 
-            Text("Version 1.0")
+            Text("Version 1.0.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
         }
