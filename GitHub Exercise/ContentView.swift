eec0cabbe1682fde36bec988a492d05c40c7dcf6
agmonster08 <26757790+agmonster08@users.noreply.github.com>
2024-10-23 16:24:34 -0400

    Version 1.0 - Red

---
+++ GitHub Exercise/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  GitHub Exercise
+//
+//  Created by Development on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }
+        .padding()
+        .foregroundStyle(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Development on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
